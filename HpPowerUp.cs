1a9f5d3f272a476ccfe064fbbe7c97d3190edd37
k1llxna <44785075+k1llxna@users.noreply.github.com>
2019-03-29 21:44:27 -0400

    Add files via upload

---
+++ HpPowerUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HpPowerUp : MonoBehaviour {
+
+    public GameObject effect;
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Player stats = col.GetComponent<Player>();
+            stats.hp = 6;
+
+            Instantiate(effect, stats.transform.position, stats.transform.rotation);
+            Destroy(gameObject);
+            Debug.Log("HP Restored!");
+        }
+    }
+}
+
+
